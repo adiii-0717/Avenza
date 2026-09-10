8555a6392d298b0cc1924e784d2e8b80855a6ac8
Aditya Manjare <adityamanjare84@gmail.com>
2026-09-10 13:50:34 +0530

    Update settings.py

--- avenza/settings.py
+++ avenza/settings.py
@@ -77,9 +77,7 @@ ASGI_APPLICATION = 'avenza.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
         },
     },
 }
